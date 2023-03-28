b7e1ad0d74698ac707e29edfd4f7f5cd75d3143c
jesopo <github@lolnerd.net>
2023-03-28 17:04:22 +0000

    better benchmarks

--- ip2asn_lib/benches/bgptools.rs
+++ ip2asn_lib/benches/bgptools.rs
@@ -1,6 +1,7 @@
+use std::net::{Ipv4Addr, Ipv6Addr};
 use criterion::{criterion_group, criterion_main, Criterion};
 use ip2asn_lib::AsnMapper as _;
-use std::net::{Ipv4Addr, Ipv6Addr};
+use rand::Rng;
 use std::path::PathBuf;
 use std::str::FromStr as _;
 
@@ -15,11 +16,14 @@ fn criterion_benchmark(c: &mut Criterion) {
     let (map_v4, map_v6) =
         ip2asn_lib::BgpTools::parse(&PathBuf::from_str("benches/bgptools.jsonl").unwrap()).unwrap();
 
-    let ipv4 = Ipv4Addr::from_str("8.8.8.8").unwrap();
-    let ipv6 = Ipv6Addr::from_str("2001:4860:4860::8888").unwrap();
+    let ipv4_cached = Ipv4Addr::from_str("8.8.8.8").unwrap();
+    let ipv6_cached = Ipv6Addr::from_str("2001:4860:4860::8888").unwrap();
+    c.bench_function("v4 cached", |b| b.iter(|| map_v4.lookup(ipv4_cached)));
+    c.bench_function("v6 cached", |b| b.iter(|| map_v6.lookup(ipv6_cached)));
 
-    c.bench_function("v4", |b| b.iter(|| map_v4.lookup(ipv4)));
-    c.bench_function("v6", |b| b.iter(|| map_v6.lookup(ipv6)));
+    let mut rng = rand::thread_rng();
+    c.bench_function("v4 uncached", |b| b.iter(|| map_v4.lookup(rng.gen::<u32>())));
+    c.bench_function("v6 uncached", |b| b.iter(|| map_v6.lookup(rng.gen::<u128>())));
 }
 
 criterion_group!(benches, criterion_benchmark);

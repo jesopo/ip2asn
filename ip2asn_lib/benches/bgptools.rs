debdae5a29105ad2972d91043b42997930139f44
jesopo <github@lolnerd.net>
2023-03-28 00:07:42 +0000

    fix benchmarks

--- ip2asn_lib/benches/bgptools.rs
+++ ip2asn_lib/benches/bgptools.rs
@@ -1,4 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use ip2asn_lib::AsnMapper as _;
 use std::net::{Ipv4Addr, Ipv6Addr};
 use std::path::PathBuf;
 use std::str::FromStr as _;
@@ -6,13 +7,13 @@ use std::str::FromStr as _;
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("load", |b| {
         b.iter(|| {
-            ip2asn_lib::bgptools::parse(&PathBuf::from_str("benches/bgptools.jsonl").unwrap())
+            ip2asn_lib::BgpTools::parse(&PathBuf::from_str("benches/bgptools.jsonl").unwrap())
                 .unwrap()
         })
     });
 
     let (map_v4, map_v6) =
-        ip2asn_lib::bgptools::parse(&PathBuf::from_str("benches/bgptools.jsonl").unwrap()).unwrap();
+        ip2asn_lib::BgpTools::parse(&PathBuf::from_str("benches/bgptools.jsonl").unwrap()).unwrap();
 
     let ipv4 = Ipv4Addr::from_str("8.8.8.8").unwrap();
     let ipv6 = Ipv6Addr::from_str("2001:4860:4860::8888").unwrap();

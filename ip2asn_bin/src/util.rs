fe849bf513f7c6d6151ab18146429a6e79565af2
jesopo <github@lolnerd.net>
2023-03-28 17:08:19 +0000

    add missing file

---
+++ ip2asn_bin/src/util.rs
@@ -0,0 +1,23 @@
+use futures::{
+    channel::mpsc::{channel, Receiver},
+    SinkExt,
+};
+use notify::{Config, Event, RecommendedWatcher, Watcher};
+
+pub(super) fn async_watcher(
+) -> notify::Result<(RecommendedWatcher, Receiver<notify::Result<Event>>)> {
+    let (mut tx, rx) = channel(1);
+
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let watcher = RecommendedWatcher::new(
+        move |res| {
+            futures::executor::block_on(async {
+                tx.send(res).await.unwrap();
+            })
+        },
+        Config::default(),
+    )?;
+
+    Ok((watcher, rx))
+}

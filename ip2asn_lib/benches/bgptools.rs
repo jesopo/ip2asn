049a0f5586de3675cc84101b4c0406064b8f66ae
jesopo <github@lolnerd.net>
2023-03-28 17:04:35 +0000

    cargo fmt

--- ip2asn_lib/benches/bgptools.rs
+++ ip2asn_lib/benches/bgptools.rs
@@ -1,7 +1,7 @@
-use std::net::{Ipv4Addr, Ipv6Addr};
 use criterion::{criterion_group, criterion_main, Criterion};
 use ip2asn_lib::AsnMapper as _;
 use rand::Rng;
+use std::net::{Ipv4Addr, Ipv6Addr};
 use std::path::PathBuf;
 use std::str::FromStr as _;
 
@@ -22,8 +22,12 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("v6 cached", |b| b.iter(|| map_v6.lookup(ipv6_cached)));
 
     let mut rng = rand::thread_rng();
-    c.bench_function("v4 uncached", |b| b.iter(|| map_v4.lookup(rng.gen::<u32>())));
-    c.bench_function("v6 uncached", |b| b.iter(|| map_v6.lookup(rng.gen::<u128>())));
+    c.bench_function("v4 uncached", |b| {
+        b.iter(|| map_v4.lookup(rng.gen::<u32>()))
+    });
+    c.bench_function("v6 uncached", |b| {
+        b.iter(|| map_v6.lookup(rng.gen::<u128>()))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
